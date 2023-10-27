14dd26f2830171011db18aac3ad961de90a7b5b3
Korjubzot <korjubzot@gmail.com>
2023-10-27 17:00:12 +1000

    Write to Desktop

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import os
 from bs4 import BeautifulSoup
 
 url = "https://archiveofourown.org/works/49442698"
@@ -6,6 +7,8 @@ response = requests.get(url)
 soup = BeautifulSoup(response.content, "html.parser")
 
 fics = soup.find_all("div", class_="userstuff")
-with open("output.txt", "w") as file:
+desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
+output_file = os.path.join(desktop_path, "output.txt")
+with open(output_file, "w") as file:
     for fic in fics:
         file.write(fic.text)
