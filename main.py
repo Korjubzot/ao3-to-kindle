1cc250fe3d2a584808738f47005db92803b1552a
Korjubzot <korjubzot@gmail.com>
2023-10-29 20:42:21 +1000

    User can input their own link

--- main.py
+++ main.py
@@ -2,13 +2,19 @@ import requests
 import os
 from bs4 import BeautifulSoup
 
-url = "https://archiveofourown.org/works/49442698"
-response = requests.get(url)
-soup = BeautifulSoup(response.content, "html.parser")
+def scraper(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
 
-fics = soup.find_all("div", class_="userstuff")
-desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
-output_file = os.path.join(desktop_path, "output.txt")
-with open(output_file, "w") as file:
-    for fic in fics:
-        file.write(fic.text)
+    fics = soup.find_all("div", class_="userstuff")
+    desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
+    output_file = os.path.join(desktop_path, "output.txt")
+    with open(output_file, "w") as file:
+        for fic in fics:
+            file.write(fic.text)
+
+# url = user input (later)
+url = input("Copy and paste a link from ao3 here:")
+
+
+scraper(url)
