ba18e07c566710bd82a20a137e0b8470ad83ad2b
Korjubzot <korjubzot@gmail.com>
2023-10-30 11:50:02 +1000

    Added basic error handling and output naming

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 import requests
 import os
 from bs4 import BeautifulSoup
+import tkinter as tk
 
-def scraper(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, "html.parser")
+def scraper(url, output_name):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.content, "html.parser")
 
-    fics = soup.find_all("div", class_="userstuff")
-    desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
-    output_file = os.path.join(desktop_path, "output.txt")
-    with open(output_file, "w") as file:
-        for fic in fics:
-            file.write(fic.text)
+        fics = soup.find_all("div", class_="userstuff")
+        if not fics:
+            raise Exception("Error: no data found. Did you copy the right link?")
+        desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
+        output_file = os.path.join(desktop_path, output_name + '.txt')
+        with open(output_file, "w") as file:
+            for fic in fics:
+                file.write(fic.text)
+        print("Scraping complete!", output_name, "is now on your Desktop.")
+    except requests.exceptions.RequestException as e:
+        print("Error retrieving data: ", e)
 
-# url = user input (later)
-url = input("Copy and paste a link from ao3 here:")
 
+url = input("Copy and paste a link from ao3 here: ")
+output_name = input("What would you like the file to be called? ")
 
-scraper(url)
+scraper(url, output_name)
